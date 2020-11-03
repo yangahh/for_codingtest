aef654714fb9b9aa737d0daf553666a119a127c3
yangahh <toaur6802@naver.com>
2020-11-03 18:37:46 +0900

    경쟁적 감염

--- DFS_BFS/BFS03.py
+++ DFS_BFS/BFS03.py
@@ -2,62 +2,52 @@
 
 
 from collections import deque
+import sys
 
-def bfs(gragh, a, b, k):
-    # 큐를 구현하기 위해 deque라이브러리 사용
-    queue = deque()
-    queue.append((a, b))
-    
-    # 방향 벡터(상, 하, 좌, 우) 설정
-    da = [-1, 1, 0, 0]
-    db = [0, 0, -1, 1]
+n , k = map(int, sys.stdin.readline().rstrip().split())
+gragh = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
+target_s, target_x, target_y = map(int, sys.stdin.readline().rstrip().split())
 
-    second = 0
-    
-    while queue:
-        a, b = queue.popleft()
-        second += 1
-        # 상, 하, 좌, 우 탐색
-        for i in range(4):
-            na = a + da[i]
-            nb = b + db[i]
-            
-            # 다음 좌표가 범위를 벗어나면 무시
-            if na < 0 or na >= n or nb < 0 or nb >= n:
-                continue
-            # 다음 좌표에 바이러스가 퍼지지 않은 경우
-            if gragh[na][nb] == 0:
-                gragh[na][nb] = k
-                queue.append((na, nb))
-         
-        # 주어진 시간이 지나면 탐색 종료
-        if second == s:
-            break
-    return gragh
-
-n , k = map(int, input().split())
-gragh = [list(map(int, input().split())) for _ in range(n)]
-s, x, y = map(int, input().split())
-
-# 각 바이러스 별 위치 정보를 저장(2차원 리스트로 [바이러스 번호, 행 번호, 열 번호]). (0행은 무시하는 행)
-virus = [[0, 0, 0]]
+# 각 바이러스 별 위치 정보를 저장(2차원 리스트로 [바이러스 번호, 초, 행 번호, 열 번호]). (초는 0으로 초기화)
+virus = []
 
 for i in range(n):
     for j in range(n):
         if gragh[i][j] != 0:
-            virus.append([gragh[i][j], i, j])
+            virus.append([gragh[i][j], 0, i, j])
 
-# 바이러스 번호 순으로 정렬
-virus.sort(key = lambda x: x[0])
+# 바이러스 번호 순으로 정렬 (첫번째 원소를 기준으로 정렬)
+virus.sort()
+# virus.sort(key = lambda x: x[0]) 같은 표현
 
-# 바이러스 번호가 작은 순서대로 탐색
-for i in range(1, k+1):
-    bfs(gragh, virus[i][1], virus[i][2], virus[i][0])
-    for xx in range(n):
-        for yy in range(n):
-            print(gragh[xx][yy], end=" ")
-        print()
-    print()
+# 바이러스 정보를 큐에 넣어 탐색
+queue = deque(virus)
 
-print(gragh[x-1][y-1])
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+while queue:
+    v_num, s, x, y = queue.popleft()
+
+    if s == target_s:
+        break
+
+    # 상, 하, 좌, 우 BFS 탐색
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        
+        # 좌표 범위를 벗어나면 무시
+        if nx < 0 or nx >= n or ny < 0 or ny >= n:
+            continue
+        
+        if gragh[nx][ny] == 0:
+            gragh[nx][ny] = v_num
+            queue.append([v_num, s+1, nx, ny])
+            
+
+
+print(gragh[target_x - 1][target_y - 1])
     
