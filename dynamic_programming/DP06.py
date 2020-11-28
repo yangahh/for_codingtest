4a3deacb7b2a1ceef24af83564ae66eb21d5de8e
yangahh <toaur6802@naver.com>
2020-11-28 20:03:52 +0900

    LIS

--- dynamic_programming/DP06.py
+++ dynamic_programming/DP06.py
@@ -1,1 +1,17 @@
-# 문제) 병사 배치하기
+# 문제) 가장 긴 증가하는 부분 수열 (https://www.acmicpc.net/problem/11053)
+
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+array = list(map(int, sys.stdin.readline().rstrip().split()))
+
+# DP테이블 초기화
+d = [1] * (n + 1)
+
+for i in range(1, n):
+    for j in range(i):
+        if array[j] < array[i]:
+            d[i] = max(d[j] + 1, d[i])
+
+
+print(max(d))
